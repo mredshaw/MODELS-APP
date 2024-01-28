5494e2e5beadbad5c2a84201478545b020fa9d5b
mredshaw <115364800+mredshaw@users.noreply.github.com>
2024-01-28 10:19:59 -0500

    commit

--- Transportation Example.py
+++ Transportation Example.py
@@ -17,7 +17,7 @@ supply = [275000, 400000, 300000]
 x = model.addVars(3, 3, lb=0, vtype=GRB.CONTINUOUS, name="Transportation Plan")
 
 # The objective function
-model.setObjective(gb.quicksum(costs[i][j]*x[i,j] for i in range(3) for j in range(3)), GRB.MINIMIZE)
+model.setObjective(gb.quicksum(costs[i][j]*x[i,j] for i in range(3) for j in range(3)), GRB.MINIMIZE) #costs is a dictionary, x is a python variable, so it gets called differenetly
 
 # Add the supply constraints
 for i in range(3):
@@ -26,6 +26,9 @@ for i in range(3):
 # Add the demand constraints
 for j in range(3):
     model.addConstr(gb.quicksum(x[i,j] for i in range(3)) <= demand[j], name="Demand Constraint %i" %j)
+
+#another approach that does the same thing
+#model.addConstr(gb.quicksum(x[i,j] for i in range(3)) <= demand[j] for j in range(3))
 
 # Optimally solve the problem
 model.optimize()
